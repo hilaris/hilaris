d3dea2a03f6cd8e005556a8eb0b7895b600834e7
oscar <oscar@oscar.(none)>
2012-03-30 12:53:12 +0200

    mutex for streamserver buffer

--- hilaris/hilaris/StreamServer.cpp
+++ hilaris/hilaris/StreamServer.cpp
@@ -1,12 +1,20 @@
 #include "StreamServer.h"
-#include <pthread.h>
 
+StreamServer::StreamServer(uint8 buffSize)
+{}
 
 bool StreamServer::start()
 {
-	message = "blubb";
-	pthread_create(&this->thread, NULL, sendData, (void*) this);
-	return true;
+	pthread_mutex_init(&this->msg_lock, NULL);
+	this->message = "blubb";
+	if(pthread_create(&this->thread, NULL, sendData, (void*) this))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 void* StreamServer::sendData(void* arg)
@@ -14,6 +22,7 @@ void* StreamServer::sendData(void* arg)
 	while(1)
 	{
 		StreamServer* s = (StreamServer*)arg;
+		s->getImage();
 		printf("message: %s\n", s->message);
 		usleep(100000);
 	}
@@ -21,8 +30,31 @@ void* StreamServer::sendData(void* arg)
 	return arg;
 }
 
+bool StreamServer::insertImage(Image img)
+{
+	pthread_mutex_lock(&this->msg_lock);
+	sleep(1);
+	//insert Image
+	pthread_mutex_unlock(&this->msg_lock);  
+	return false;
+}
+
+Image StreamServer::getImage()
+{
+	pthread_mutex_lock(&this->msg_lock);
+	sleep(1);
+	//get Image from buffer
+	pthread_mutex_unlock(&this->msg_lock);  
+	Image i(752, 480, OSC_PICTURE_BGR_24);
+	return i;
+}
+
+
+
 bool StreamServer::stop()
 {
 	pthread_cancel(this->thread);
+	pthread_join(this->thread, NULL);
+	pthread_mutex_destroy(&this->msg_lock);
 	return true;
 }
--- hilaris/hilaris/includes/StreamServer.h
+++ hilaris/hilaris/includes/StreamServer.h
@@ -7,12 +7,20 @@
 
 class StreamServer {
 	public:
+		StreamServer(uint8 buffSize);
 		bool start();
 		static void *sendData(void* arg);
 		bool stop();
-		char* message;
+		bool insertImage(Image img);
+		Image getImage();
+		
 	private:
 		pthread_t thread;
+		pthread_mutex_t msg_lock;
+		char* message;
+		Image* images;
+		int startBuff;
+		int endBuff;
 };
 
 #endif 
