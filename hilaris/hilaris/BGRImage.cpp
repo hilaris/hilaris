880e27786b30453d031e83226d7a20a1aae7666d
oscar <oscar@oscar.(none)>
2012-04-03 16:36:58 +0200

    bgr image traversal, holy malony, not quite correct yet

--- hilaris/hilaris/BGRImage.cpp
+++ hilaris/hilaris/BGRImage.cpp
@@ -18,3 +18,27 @@ uint8* BGRImage::getDataPtr()
 	return this->data;
 }
 
+void BGRImage::pixel(uint16 x, uint16 y, struct BGRPixel& pixel)
+{
+	pixel.blue  = this->data[x * this->getWidth() * 3 + y * 3];
+	pixel.green = this->data[x * this->getWidth() * 3 + y * 3 + 1];
+	pixel.red   = this->data[x * this->getWidth() * 3 + y * 3 + 1 + 1];
+}
+
+BGRPixel& BGRImage::operator()(const uint16 x, const uint16 y, struct BGRPixel& pixel)
+{
+    this->pixel(x, y, pixel);
+    
+    return pixel;
+}
+
+uint8& BGRImage::component(uint16 x, uint16 y)
+{
+	return this->data[x * this->getWidth() * 3 + y];
+}
+
+uint8& BGRImage::operator()(const uint16 x, const uint16 y)
+{
+    return this->component(x, y);
+}
+
--- hilaris/hilaris/includes/BGRImage.h
+++ hilaris/hilaris/includes/BGRImage.h
@@ -4,6 +4,13 @@
 #include "oscar.h"
 #include "Image.h"
 
+struct BGRPixel
+{
+	uint8 blue;
+	uint8 green;
+	uint8 red;
+};
+
 class BGRImage : public Image
 {
 	public:
@@ -11,6 +18,14 @@ class BGRImage : public Image
 		
 		EnOscPictureType getType();
 		uint8* getDataPtr();
+		
+		void pixel(uint16 x, uint16 y, struct BGRPixel& pixel);
+		uint8& component(uint16 x, uint16 y);
+		
+		// operators
+		// access via: image(x, y) = pixelvalue;
+		BGRPixel& operator()(const uint16 x, const uint16 y, struct BGRPixel& pixel);
+		uint8&    operator()(const uint16 x, const uint16);
 	
 	private:
 		uint8 data[OSC_CAM_MAX_IMAGE_WIDTH * OSC_CAM_MAX_IMAGE_HEIGHT * 3];
