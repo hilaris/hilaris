827741788c7a6f17c09aae37f4a617b0e01f6112
Jim Schmid <devsheeep@gmail.com>
2012-04-15 10:16:40 +0200

    documented DebayerBGRBilinear

--- hilaris/hilaris/includes/DebayerBGRFast.h
+++ hilaris/hilaris/includes/DebayerBGRFast.h
@@ -10,15 +10,24 @@
 #include "BGRImageFactory.h"
 #include "Debayer.h"
 
+/**
+ *  @brief Bilinear Debayer for BGRImage.
+ *
+ *  @author Jim Schmid
+ *  @author Michael Kreis
+ *
+ *  @version 1.0
+ *  @since March 2012
+ */
 class DebayerBGRFast : public Debayer
 {
 	public:
-		DebayerBGRFast(){}
 		enum EnOscPictureType getType();
 		BGRImage* getObject(uint16 width, uint16 height);
 		bool debayer(RawImage* raw, Image* image);
 		long unsigned int getSize();
 		
+		DebayerBGRFast(){}
 		virtual ~DebayerBGRFast() {};
 };
 
