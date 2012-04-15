39ddd3c5630cbeacd0d9c157dbd20ad9eb5c476f
Jim Schmid <devsheeep@gmail.com>
2012-04-15 09:16:38 +0200

    documented BGRImageFactory

--- hilaris/hilaris/includes/BGRImageFactory.h
+++ hilaris/hilaris/includes/BGRImageFactory.h
@@ -22,13 +22,84 @@ class RGBImage;
 class BGRImageFactory
 {
 	public:
+	
+		/**
+		 *  @brief Get a BGRImage out of a RawImage and its data using the fast
+		 *   debayer.
+		 *
+		 *  @param raw The RawImage from the Camera.
+		 *  @return The debayered BGRImage.
+		 *
+		 *  @warning This function is used internaly in Hilaris and does not have
+		 *   to be invoked by a user.
+		 */
 		static BGRImage getFastDebayered(RawImage* raw);
+		
+		/**
+		 *  @brief Get a BGRImage out of a RawImage and its data using a bilinear
+		 *   debayer.
+		 *
+		 *  @param raw The RawImage from the Camera.
+		 *  @return The debayered BGRImage.
+		 *
+		 *  @warning This function is used internaly in Hilaris and does not have
+		 *   to be invoked by a user.
+		 */
 		static BGRImage getBilinearDebayered(RawImage* raw);
+		
+		/**
+		 *  @brief Get a BGRImage out of a RawImage and its data using the standard
+		 *   debayer.
+		 *
+		 *  @param raw The RawImage from the Camera.
+		 *  @return The debayered BGRImage.
+		 *
+		 *  @warning This function is used internaly in Hilaris and does not have
+		 *   to be invoked by a user.
+		 */
 		static BGRImage getDebayered(RawImage* raw);
+		
+		/**
+		 *  @brief Get a BGRImage out of a RawImage and its data using a halfsize
+		 *   debayer.
+		 *
+		 *  @param raw The RawImage from the Camera.
+		 *  @return The debayered BGRImage.
+		 *
+		 *  @warning This function is used internaly in Hilaris and does not have
+		 *   to be invoked by a user.
+		 */
 		static BGRImage getHalfsizeDebayered(RawImage* raw);
 		
+		
+		/**
+		 *  @brief Get a BGRImage from a RGBImage swapping the color components.
+		 *
+		 *  @param rgb An input RGBImage.
+		 *  @return The resulting BGRImage.
+		 *
+		 *  @see BGRImageFactory::getFromBinaryImage for an example of usage.
+		 */
 		static BGRImage getFromRGBImage(RGBImage& rgb);
+		
+		/**
+		 *  @brief Get a BGRImage from a GreyscaleImage.
+		 *
+		 *  @param grey An input GreyscaleImage.
+		 *  @return The resulting BGRImage.
+		 *
+		 *  @see BGRImageFactory::getFromBinaryImage for an example of usage.
+		 */
 		static BGRImage getFromGreyscaleImage(GreyscaleImage& grey);
+		
+		/**
+		 *  @brief Get a BGRImage from a BinaryImage.
+		 *
+		 *  @param binary An input BinaryImage.
+		 *  @return The resulting BGRImage.
+		 *
+		 *  @include BGRImageFactoryFromBinaryImage.cpp
+		 */
 		static BGRImage getFromBinaryImage(BinaryImage& binary);
 		
 		/**
@@ -36,9 +107,9 @@ class BGRImageFactory
 		 *
 		 *  @param width  The width of the resulting image.
 		 *  @param height The height of the resulting image.
-		 *  @param blue   Blue color Component of filling color.
-		 *  @param green  Green color Component of filling color.
-		 *  @param red    Red color Component of filling color.
+		 *  @param blue   Blue color component of fill color.
+		 *  @param green  Green color component of fill color.
+		 *  @param red    Red color component of fill color.
 		 *
 		 *  @return A BGRImage.
 		 *
