ebcbba75d1c64e4a655edcdff5b2bced9ff6f35a
Michael Kreis <michi.kreis@gmail.com>
2012-04-17 14:57:11 +0200

    fixed streamserver

--- hilaris/hilaris/ImageBuffer.cpp
+++ hilaris/hilaris/ImageBuffer.cpp
@@ -3,20 +3,15 @@
 
 ImageBuffer::ImageBuffer(int dataSize, int bufferSize)
 {
+	printf("%d\n", dataSize);
 	this->dataSize = dataSize;
 	this->bufferSize = bufferSize;
 	
 	this->count = 0;
 	this->start = 0;
-	
-	Debug::log("count %d start %d\n", this->count, this->start);
-	
+		
 	this->data = new uint8[this->dataSize * this->bufferSize];
-	Debug::log("count %d start %d\n", this->count, this->start);
 	this->currentImg = new uint8[this->dataSize];
-	
-	Debug::log("count %d start %d\n", this->count, this->start);
-	Debug::log("count %p start %p data %p\n", &this->count, &this->start, this->data);
 }
 
 ImageBuffer::~ImageBuffer()
@@ -39,8 +34,8 @@ void ImageBuffer::insert(uint8* img)
 {
 	this->mutex.lock();
 	{
-		int end = (this->start - this->count) % this->bufferSize;
-		Debug::log("start %d count %d\n", this->start, this->count);
+		int end = (this->start + this->count) % this->bufferSize;
+		printf("start %d count %d insert %d\n", this->start, this->count, end);
 		memcpy(&this->data[end*this->dataSize], img, this->dataSize);
 	
 		if(this->count == this->bufferSize)
@@ -59,7 +54,7 @@ uint8* ImageBuffer::get()
 {
 	this->mutex.lock();
 	{
-		Debug::log("position insert %d\n", this->start*this->dataSize);
+		printf("position insert %d\n", this->start*this->dataSize);
 		memcpy(this->currentImg, &this->data[this->start*this->dataSize], this->dataSize);
 		
 		this->start = (this->start + 1) % this->bufferSize;
