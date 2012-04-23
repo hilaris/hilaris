314c92c80f8f16001ba5c9ea02c98c813a3370e1
Jim Schmid <devsheeep@gmail.com>
2012-04-23 18:19:55 +0200

    ov init

--- hilaris/hilaris/Box.cpp
+++ hilaris/hilaris/Box.cpp
@@ -2,5 +2,6 @@
 
 void Box::draw(Image* image)
 {
+	ov_init();
 	ov_box(&image->getOscarContext(), this->x1, this->y1, this->x2, this->y2, this->color, this->fill);
 }
--- hilaris/hilaris/Text.cpp
+++ hilaris/hilaris/Text.cpp
@@ -2,7 +2,8 @@
 
 void Text::draw(Image* image)
 {
-	ov_text(&image->getOscarContext(), this->x, this->y, this->size, &this->text[0], this->fgColor, this->bgColor);
+	ov_init();
+	ov_text(&image->getOscarContext(), this->x, this->y, this->size, this->text, this->fgColor, this->bgColor);
 }
 
 Text::Text(uint16 x, uint16 y, uint8 size, const char* text, uint32 fgColor, uint32 bgColor) : x(x), y(y), size(size), fgColor(fgColor), bgColor(bgColor)
