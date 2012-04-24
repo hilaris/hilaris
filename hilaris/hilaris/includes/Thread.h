129dc0cfa46129523cb0cab21d1e7d1223c2cc81
Jim Schmid <devsheeep@gmail.com>
2012-04-24 16:40:01 +0200

    pong startup screen

--- hilaris/hilaris/includes/Thread.h
+++ hilaris/hilaris/includes/Thread.h
@@ -13,12 +13,35 @@ class Thread
 		static void* dispatch(void *);
 		volatile sig_atomic_t cancel;
 	protected:
+		/**
+		 *  @brief This method is the main run method of the Thread. Has to be
+		 *  implemented by subinstance.
+		 */
 		virtual void run() = 0;
 	public:
+		/**
+		 *  @brief Create a Thread. This is a constructor.
+		 */
 		Thread();
+		
+		/**
+		 *  @brief Destructor for a specific Thread. Has to be implemented.
+		 */
 		virtual ~Thread();
+		
+		/**
+		 *  @brief Start the Thread.
+		 */
 		void start();
+		
+		/**
+		 *  @brief Wait for this Thread to be finsihed.
+		 */
 		void join();
+		
+		/**
+		 * @brief Stop this Thread.
+		 */
 		void stop();
 };
 
