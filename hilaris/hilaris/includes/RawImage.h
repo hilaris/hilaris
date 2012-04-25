605223bde5c2c07327e16fb4cae54cc5de6ec6cd
Jim Schmid <devsheeep@gmail.com>
2012-04-25 11:18:38 +0200

    more docs

--- hilaris/hilaris/includes/RawImage.h
+++ hilaris/hilaris/includes/RawImage.h
@@ -35,28 +35,163 @@ class RawImage : public Image
 		 */
 		RawImage(uint16 width, uint16 height);
 		
+		/**
+		 *  @brief Get the image type.
+		 *  @return The image type.
+		 */
 		EnOscPictureType getType();
+		
+		/**
+		 *  @brief Return a pointer to the underlying image data.
+		 *  @return dataptr The data pointer.
+		 */
 		uint8* getDataPtr();
 		
+		/**
+		 *  @brief Debayer this RawImage to a given GreyscaleImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A GreyscaleImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(GreyscaleImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given GreyscaleImage using a
+		 *   vector based debayer.
+		 *
+		 *  @param image A GreyscaleImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerVector(GreyscaleImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given GreyscaleImage using a
+		 *   halfsize debayer.
+		 *
+		 *  @param image A GreyscaleImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerHalfsize(GreyscaleImage* image);
 		
+		/**
+		 *  @brief Debayer this RawImage to a given BGRImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A BGRImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(BGRImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given BGRImage using a
+		 *   bilinear debayer.
+		 *
+		 *  @param image A BGRImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerBilinear(BGRImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given BGRImage using the
+		 *   standard debayer.
+		 *
+		 *  @param image A BGRImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerStandard(BGRImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given BGRImage using a
+		 *   halfsize debayer.
+		 *
+		 *  @param image A BGRImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerHalfsize(BGRImage* image);
 		
+		/**
+		 *  @brief Debayer this RawImage to a given RGBImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A RGBImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(RGBImage* image);
 		
+		/**
+		 *  @brief Debayer this RawImage to a given ComponentHImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A ComponentHImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(ComponentHImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given ComponentSImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A ComponentSImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(ComponentSImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given ComponentLImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A ComponentLImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(ComponentLImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given ComponentYImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A ComponentYImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(ComponentYImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given ComponentUImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A ComponentUImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(ComponentUImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given ComponentVImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A ComponentVImage pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(ComponentVImage* image);
+		
+		/**
+		 *  @brief Debayer this RawImage to a given YUV422Image using a
+		 *   fast debayer.
+		 *
+		 *  @param image A YUV422 pointer.
+		 *  @return Success state.
+		 */
 		bool debayerFast(YUV422Image* image);
 		
+		/**
+		 *  @brief Debayer this RawImage to a given BinaryImage using a
+		 *   fast debayer.
+		 *
+		 *  @param image A BinaryImage pointer.
+		 *  @param threshold The threshold which should be used.
+		 *  @param blackIsForeground Wheter black or white is the foreground color.
+		 *
+		 *  @return Success state.
+		 */
 		bool debayerFast(BinaryImage* image, uint8 threshold = 127, bool blackIsForeground = false);
 		
 		
